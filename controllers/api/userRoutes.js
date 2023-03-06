c6740beb0dda863913dfd0020a2a85d52955bb63
brandongiordano <bgiordano9917@aol.com>
2023-03-06 17:41:09 -0500

    commit

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -58,4 +58,13 @@ router.post('/logout', (req, res) => {
   }
 });
 
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
